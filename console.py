cf591a2fb57f8ce3ea37a3868dd930e82588c69a
Midorichie <hamsohood@gmail.com>
2024-03-05 22:22:20 +0100

    Update

--- console.py
+++ console.py
@@ -177,3 +177,120 @@ class_mapping = {
             return
 
         cls_name = args[0]
+
+        if cls_name not in HBNBCommand.class_mapping:
+            print("** class doesn't exist **")
+            return
+        
+        instance_id = args[1]
+
+        instance_key = '{}.{}'.format(cls_name, instance_id)
+        all_objs = models.storage.all()
+
+        if instance_key not in all_objs.keys():
+            print("** no instance found **")
+            return
+
+        if len(args) == 2:
+            print("** attribute name missing **")
+            return
+
+        if len(args) == 3:
+            print("** value missing **")
+            return
+
+        attribute_name = args[2]
+        attribute_value = args[3]
+
+        attribute_type = type(eval(attribute_value))
+
+        if attribute_type not in (str, int, float):
+            return
+        setattr(all_objs[instance_key], attribute_name, eval(attribute_value))
+        models.storage.save()
+
+    def help_update(self):
+        """Thedocumentation for when 'help update' is called"""
+        print('Updates an instance based on the class name and '
+            'id by adding or updating attribute (save the '
+            'change into the JSON file).'
+            'Ex: $ update BaseModel 1234-1234-1234 email "aibnb@mail.com" \n'
+            )
+
+    def do_count(self, line):
+        """Count instances
+        This module returns the count of instances saved
+
+        Args:
+            line: this is expected to be the command,
+                (count User OR User.count())
+
+        Return:
+            Prints the count of the instances of the class saved.
+            or Appropriate error message
+            Return on either success or failure
+        """
+
+        count = 0
+        args = line.split()
+        if len(args) == 1:
+            cls_name = args[0]
+            if cls_name not in HBNBCommand.class_mapping:
+                print("** class doesn't exist **")
+                return
+
+            all_objs = models.storage.all()
+            for obj in all_objs.values():
+                if type(obj).__name__ == cls_name:
+                    count = count + 1
+            print(count)
+            return
+        else:
+            print("** class name missing **")
+            return
+
+    def help_count(self):
+        """The documentation on how to use count"""
+        print("\n".join([
+                        "USAGE: ",
+                        "1. count [User]: Replace user with valid class name",
+                        "2. User.count(): Replace user with valid class name",
+                        " ",
+                        "prints number of instances of the cls_name saved"]))
+
+    def do_quit(self, line):
+        """This method is called when the quit is triggered.
+        The program exits in a clean way.
+        Args:
+        line: the line being read at the moment
+        """
+        print()
+        return True
+
+    def help_quit(self):
+        """The documentation for when 'help quit' is called"""
+        print('Quit command to exit the program\n')
+
+    def do_EOF(self, line):
+        """This method is called when the EOF is triggered.
+        The program exits in a clean way.
+        Args:
+        line: the line being read at the moment
+        """
+        print()
+        return True
+
+    def help_EOF(self):
+        """The documentation for when 'help EOF' is called"""
+        print('EOF command to exit the program\n')
+
+    def emptyline(self):
+        """When an empty line + ENTER
+        shouldn't execute anything
+        """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
